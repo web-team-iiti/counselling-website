9f82fd195dd8a225e162ce6caa20515c362d6bcb
ShagunSG <shaguneghatak@gmail.com>
2023-02-06 14:51:35 +0530

    final navbar changes done

--- responsive.js
+++ responsive.js
@@ -1,14 +1,17 @@
+// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
-        
-var navlist = document.getElementById("Sticky");
-var sticky = navlist.offsetTop;
 
-/* Function to stick the nav bar */
+// Get the navbar
+var navbar = document.getElementById("Navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navlist.classList.add("sticky")
-    }
-    else {
-        navlist.classList.remove("sticky");
-    }
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
