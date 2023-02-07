8f179509f4075996265bb68b1b43d54edda86ba3
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-02-08 02:26:15 +0530

    navbar changes

--- responsive.js
+++ responsive.js
@@ -1,17 +1,27 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction(); console.log("hi")};
 
 // Get the navbar
 var navbar = document.getElementById("Navbar");
+/* var button = document.getElementById("b1");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
+button.onclick = function(){
+  if(window.pageYOffset > 10 + prevScroll){
+    navbar.style.display="none";
+  }
+  else{
+    navbar.style.display="block";
+  }
+} */
+var prevScroll = window.pageYOffset
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
+    if (window.pageYOffset < prevScroll-10) {
+      navbar.style.top="0";
+      navbar.style.padding="8px";
+    } else if(window.pageYOffset > 10 + prevScroll){
+      navbar.style.top="-112px";
+      navbar.style.padding="0px";
+    }
+    prevScroll=window.pageYOffset;
 }
